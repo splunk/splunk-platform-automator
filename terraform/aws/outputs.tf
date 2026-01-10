7d4cdafb711193721a3f7ddebb61dc8e6f4188b4
Marco Stadler <mstadler@splunk.com>
2026-01-10 16:55:51 +0100

    changed to json payload

--- terraform/aws/outputs.tf
+++ terraform/aws/outputs.tf
@@ -1,14 +1,14 @@
-output "instance_ids" {
-  description = "Map of hostname to instance ID"
-  value       = { for k, v in aws_instance.splunk : k => v.id }
-}
-
-output "public_ips" {
-  description = "Map of hostname to public IP address"
-  value       = { for k, v in aws_instance.splunk : k => v.public_ip }
-}
-
-output "private_ips" {
-  description = "Map of hostname to private IP address"
-  value       = { for k, v in aws_instance.splunk : k => v.private_ip }
+output "ansible_inventory" {
+  description = "Ansible-compatible inventory in JSON format with all host details"
+  value = jsonencode({
+    for hostname, instance in aws_instance.splunk : hostname => {
+      ansible_host                 = instance.public_dns
+      ansible_user                 = local.ssh_username
+      ansible_ssh_private_key_file = var.ssh_private_key_file
+      private_dns_name             = instance.private_dns
+      private_ip                   = instance.private_ip
+      public_dns_name              = instance.public_dns
+      public_ip                    = instance.public_ip
+    }
+  })
 }
